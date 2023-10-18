0c97fc35c28b2b9c7858935e782388922d405371
rauchips <chipindebenjamin@gmail.com>
2023-10-18 21:21:32 +0300

    get method webhook simple setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,27 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/webhook')
+  getVerified(
+    @Query() query: Record<string, any>,
+    @Res() res: Response,
+  ): Response {
+    console.log(query);
+    console.log(query["hub.mode"]);
+
+    const token = "meatyhamhock";
+    const mode: string = query["hub.mode"];
+    const challenge: string = query["hub.challenge"];
+    const verify_token: string = query["hub.verify_token"];
+
+    if(mode === 'subscribe' && token === verify_token) {
+      console.log("WEBHOOK_VERIFIED");
+      return res.status(200).send(challenge);
+    }
+    
+    return res.status(400).send(new Error('Error occured.'));
+    
   }
 }
